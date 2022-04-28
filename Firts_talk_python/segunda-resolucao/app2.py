5ff8f3e0f98d55c7add475152daa0ed1ba9fade2
VitorPandini <valvespandini@gmail.com>
2022-04-28 17:02:38 -0300

    Adição de comentários, para deixar mais explicativo

--- Firts_talk_python/segunda-resolucao/app2.py
+++ Firts_talk_python/segunda-resolucao/app2.py
@@ -3,11 +3,14 @@ import pygame
 import os
 from playsound import playsound
 
+# Importação bibliotecas:gtts,pygame,os e playsound
 
+#função para armazenar o audio, como o proprio nome sugere
 def guardar_audio(text):
     var = gTTS(text = text, lang = 'pt-br')
     var.save('audio.mp3')
 
+#função para tocar o audio em formato mp3, as linhas comentadas sugerem que pode ser feita tanto com a biblioteca pygame tanto quanto a playsound
 def tocar_mp3():
     playsound('audio.mp3')
     #pygame.mixer.init()
@@ -18,6 +21,7 @@ def tocar_mp3():
     os.remove('./audio.mp3')
 
 text = ''
+#text, inicia uma variavel vazia, abaixo utiização de um loop para efetuar as tarefas assim sugeridas
 while text != 'sair':
     text = input(': ')
     guardar_audio(text)
